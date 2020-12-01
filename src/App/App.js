1962f659ca0df4bb90b13b7587aaec7a303f61f8
uimm258 <uimm258@gmail.com>
2020-12-01 02:26:09 -0800

    ..

--- src/App/App.js
+++ src/App/App.js
@@ -20,8 +20,8 @@ class App extends Component {
 
     componentDidMount() {
         Promise.all([
-            fetch(`${config.API_ENDPOINT}/notes`),
-            fetch(`${config.API_ENDPOINT}/folders`)
+            fetch(`${config.API_ENDPOINT}/note`),
+            fetch(`${config.API_ENDPOINT}/folder`)
         ])
             .then(([notesRes, foldersRes]) => {
                 if (!notesRes.ok)
